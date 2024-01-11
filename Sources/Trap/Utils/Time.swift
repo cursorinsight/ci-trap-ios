ecfd68526d2080481bed0168accc9a589142f5b2
Denes <denes@cursorinsight.com>
2024-01-11 16:55:26 +0100

    fix: Fix event timestamps
    Do not use the bootTime as that can cause the sensor and touch
    timestamps to become incorrect.

--- Sources/Trap/Utils/Time.swift
+++ Sources/Trap/Utils/Time.swift
@@ -5,16 +5,16 @@ class TrapTime {
 
     public static func normalizeTime(_ time: TimeInterval) -> Int64 {
         if bootTime == nil {
-            // Query system boot time
-            var tv = timeval()
-            var tvSize = MemoryLayout<timeval>.size
-            let err = sysctlbyname("kern.boottime", &tv, &tvSize, nil, 0)
-            if err == 0, tvSize == MemoryLayout<timeval>.size {
-                bootTime = Date(timeIntervalSince1970: Double(tv.tv_sec) + Double(tv.tv_usec) / 1_000_000.0)
-            } else {
-                debugPrint("Cannot load the system boot time for touch precision timestamp")
+            // // Query system boot time
+            // var tv = timeval()
+            // var tvSize = MemoryLayout<timeval>.size
+            // let err = sysctlbyname("kern.boottime", &tv, &tvSize, nil, 0)
+            // if err == 0, tvSize == MemoryLayout<timeval>.size {
+            //     bootTime = Date(timeIntervalSince1970: Double(tv.tv_sec) + Double(tv.tv_usec) / 1_000_000.0)
+            // } else {
+            //     debugPrint("Cannot load the system boot time for touch precision timestamp")
                 bootTime = Date() - ProcessInfo().systemUptime
-            }
+            // }
         }
 
         return Int64((bootTime!.timeIntervalSince1970 + time) * 1000)
