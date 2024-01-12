89d883aca04036c8204826a5e9357bc8ce083e7d
Denes Sapi <denes@cursorinsight.com>
2024-01-12 18:32:56 +0100

    fix: Fix switching between collector modes
    Always restart the collection if the battery or network status changes.

--- Tests/TrapTests/Manager/ManagerTests.swift
+++ Tests/TrapTests/Manager/ManagerTests.swift
@@ -9,10 +9,10 @@ class ManagerTests: XCTestCase {
         MockCollector.stopCalled = { stopCalled.fulfill() }
         var config = TrapConfig()
         config.defaultDataCollection.collectors = []
-        var manager: TrapManager? = try TrapManager(withConfig: config, withReporterQueue: nil, withCollectorQueue: nil)
+        let manager: TrapManager = TrapManager(withConfig: config, withReporterQueue: nil, withCollectorQueue: nil)
         let mockCollector = MockCollector()
-        try manager!.run(collector: mockCollector)
-        manager?.halt(collector: mockCollector)
+        try manager.run(collector: mockCollector)
+        manager.halt(collector: mockCollector)
 
         wait(for: [startCalled, stopCalled], timeout: 1)
     }
@@ -24,10 +24,10 @@ class ManagerTests: XCTestCase {
         MockCollector.stopCalled = { stopCalled.fulfill() }
         var config = TrapConfig()
         config.defaultDataCollection.collectors = [String(reflecting: MockCollector.self)]
-        var manager: TrapManager? = try TrapManager(withConfig: config, withReporterQueue: nil, withCollectorQueue: nil)
-        try manager?.runAll()
-        wait(for: [startCalled], timeout: 5)
-        manager?.haltAll()
+        let manager: TrapManager = TrapManager(withConfig: config, withReporterQueue: nil, withCollectorQueue: nil)
+        try manager.runAll()
+        wait(for: [startCalled], timeout: 10)
+        manager.haltAll()
         wait(for: [stopCalled], timeout: 1)
     }
 }
